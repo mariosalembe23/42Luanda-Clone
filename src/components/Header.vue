aa50681d3b40164c1774ca4ca70939c61b23baa0
Mário Lino Salembe <linomario199010@gmail.com>
2023-09-01 17:41:28 +0100

    Header e OffCanvas Aprimorado

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -42,25 +42,64 @@
               </li>
               <li>
                 <div
-                  class="px-4 md:border-l border-white border-opacity-60 flex items-center space-x-3 md:space-x-5"
+                  class="px-4 md:border-l border-white border-opacity-60 flex items-center space-x-3 md:space-x-4"
                 >
                   <a
                     href="#"
                     class="text-cyan-100 transition-all hover:text-cyan-300"
                   >
-                    <i class="bi bi-door-open"></i>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                      />
+                    </svg>
                   </a>
                   <a
                     href="#"
                     class="text-cyan-100 md:inline-block hidden transition-all hover:text-cyan-300"
                   >
-                    <i class="bi bi-bell"></i>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"
+                      />
+                    </svg>
                   </a>
                   <a
                     href="#"
                     class="text-cyan-100 md:inline-block hidden transition-all hover:text-cyan-300"
                   >
-                    <i class="bi bi-instagram"></i>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+                      />
+                    </svg>
                   </a>
                   <button
                     @click="OpenOffCanvas"
@@ -103,24 +142,65 @@
             </p>
           </div>
           <div
-            class="flex md:flex-row flex-col items-center justify-center mt-5 gap-3"
+            class="flex lg:flex-row flex-col items-center justify-center mt-5 gap-3"
           >
             <a
               href="#"
               class="bg-cyan-300 w-full md:w-40 rounded-md flex items-center justify-center py-3 px-3 text-zinc-950 font-semibold transition-all hover:bg-cyan-200"
-              >Entrar <i class="bi bi-door-open ms-1"></i
+              >Entrar
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5 ms-1"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                /></svg
             ></a>
             <a
-              href="#"
+              href="#FAQ"
               class="bg-zinc-950 w-full md:w-40 rounded-md flex items-center justify-center py-3 px-3 text-cyan-300 font-semibold transition-all hover:bg-zinc-900"
-              >FAQ <i class="bi bi-question-circle ps-1"></i
-            ></a>
+              >FAQ
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5 ms-1"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+                />
+              </svg>
+            </a>
           </div>
         </div>
       </section>
     </div>
     <!-- ICON FLUTUANT -->
-    <i class="bi bi-chevron-double-down text-white absolute iconFloat"> </i>
+
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-8 h-8 absolute iconFloat text-white"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"
+      />
+    </svg>
   </header>
 
   <offcanvas />
@@ -151,12 +231,11 @@ function OpenOffCanvas() {
   const offCanvas = document.querySelector(".offcanvas");
 
   offCanvas.classList.add("showOffCanvas");
-  document.body.style.overflow = 'hidden'
+  document.body.style.overflow = "hidden";
 }
 </script>
 
 <style scoped>
-
 .iconFloat {
   font-size: 2.4rem;
   transform: translate(-50%, -50%);
--- src/components/Offcanvas.vue
+++ src/components/Offcanvas.vue
@@ -3,15 +3,28 @@ function closeOffCanvas() {
   const offCanvas = document.querySelector(".offcanvas");
 
   offCanvas.classList.remove("showOffCanvas");
-  document.body.style.overflow = 'auto'
+  document.body.style.overflow = "auto";
 }
 </script>
 <template>
   <div class="offcanvas z-30 p-6 relative lg:hidden inline-block">
     <header class="flex items-center w-full justify-between">
       <span class="menu text-white font-semibold text-xl">Menu</span>
-      <button class="text-2xl" @click="closeOffCanvas">
-        <i class="bi bi-arrow-bar-right text-red-300"></i>
+      <button class="text-red-200" @click="closeOffCanvas">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
       </button>
     </header>
     <main class="mt-8">
@@ -19,37 +32,126 @@ function closeOffCanvas() {
         <ul class="flex flex-col space-y-6">
           <li class="flex items-center justify-between w-full">
             <a
-            @click="closeOffCanvas"
+              @click="closeOffCanvas"
               href="#topicos"
-              class="offLink transition-all hover:text-white text-cyan-100 font-medium"
-              >Sobre</a
+              class="offLink transition-all hover:text-white text-cyan-50 font-medium flex items-center gap-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+                />
+              </svg>
+
+              Sobre</a
             >
             <i class="bi bi-chevron-right text-cyan-100"></i>
           </li>
           <li class="flex items-center justify-between w-full">
             <a
-            @click="closeOffCanvas"
+              @click="closeOffCanvas"
               href="#vantagens"
-              class="offLink transition-all hover:text-white text-cyan-100 font-medium"
-              >Vantagens</a
+              class="offLink transition-all hover:text-white text-cyan-50 font-medium flex items-center gap-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+                />
+              </svg>
+
+              Vantagens</a
             >
             <i class="bi bi-chevron-right text-cyan-100"></i>
           </li>
           <li class="flex items-center justify-between w-full">
             <a
-            @click="closeOffCanvas"
+              @click="closeOffCanvas"
               href="#comunidade"
-              class="offLink transition-all hover:text-white text-cyan-100 font-medium"
-              >Comunidade</a
+              class="offLink transition-all hover:text-white text-cyan-50 font-medium flex items-center gap-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+                />
+              </svg>
+
+              Comunidade</a
             >
             <i class="bi bi-chevron-right text-cyan-100"></i>
           </li>
           <li class="flex items-center justify-between w-full">
             <a
-            @click="closeOffCanvas"
+              @click="closeOffCanvas"
               href="#FAQ"
-              class="offLink transition-all hover:text-white text-cyan-100 font-medium"
-              >FAQ</a
+              class="offLink transition-all hover:text-white text-cyan-50 font-medium flex items-center gap-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+                />
+              </svg>
+
+              FAQ</a
+            >
+            <i class="bi bi-chevron-right text-cyan-100"></i>
+          </li>
+          <li class="flex items-center justify-between w-full">
+            <a
+              @click="closeOffCanvas"
+              href="#FAQ"
+              class="offLink transition-all hover:text-white text-cyan-50 font-medium flex items-center gap-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"
+                />
+              </svg>
+
+              Notificações</a
             >
             <i class="bi bi-chevron-right text-cyan-100"></i>
           </li>
@@ -88,9 +190,9 @@ function closeOffCanvas() {
   font-family: "Poppins", sans-serif;
   font-size: 15px;
 }
-@media screen and (max-width:640px) {
-    .offcanvas{
-        width: 100vw;
-    }
+@media screen and (max-width: 640px) {
+  .offcanvas {
+    width: 100vw;
+  }
 }
 </style>
